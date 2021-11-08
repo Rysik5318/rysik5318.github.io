245b871fcdc3a8b62dc25e8b14aa0be0bab42ccb
Rysik5318 <72207886+Rysik5318@users.noreply.github.com>
2021-11-08 17:30:16 +0400

    Add files via upload

---
+++ test/assets/js/js.js
@@ -0,0 +1,9 @@
+let x = 0;
+
+function f1(x) {
+	let input = document.getElementById("input");
+	let input2 = document.getElementById("input-1");
+	input2.value = input.value * 500 + " Рублей";
+}
+
+button.addEventListener("click", f1)
